3364a402a45e358021c81c4c139817bcd4c4d630
caizhibiao <caizhibiao@cmhi.chinamobile.com>
2020-12-01 09:58:16 +0800

    OCT init

---
+++ alidemo-api/src/main/java/api/UserService.java
@@ -0,0 +1,14 @@
+package com.xiaoyu.alidemo.api;
+
+
+import com.xiaoyu.alidemo.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
---
+++ start/src/main/java/com/xiaoyu/alidemo/AlidemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.alidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlidemoApplication.class, args);
+    }
+
+}
