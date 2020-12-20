de467639626a6eb199b4976fdbe7bf14dc7c8c46
caizhibiao <caizhibiao@cmhi.chinamobile.com>
2020-12-20 09:53:26 +0800

    添加数据比对模块

---
+++ alidemo-comparison/src/main/java/com/xiaoyu/alidemo/comparison/ComparisionApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.alidemo.comparison.comparision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComparisionApplication.class, args);
+    }
+
+}
---
+++ alidemo-comparison/src/test/java/com/xiaoyu/alidemo/comparison/comparision/ComparisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.alidemo.comparison.comparision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
